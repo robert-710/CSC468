bec68ce67d5be97c7fa01a2f86ce67443b076f33
Robert Silver <143738061+robert-710@users.noreply.github.com>
2024-02-21 21:03:27 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -8,7 +8,7 @@ request = pc.makeRequestRSpec()
 
 tourDescription = \
 """
-This profile provides the template for a compute node with Docker installed on Ubuntu 22
+This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
 """
 
 #
@@ -26,7 +26,7 @@ node.routable_control_ip = "true"
 bs_landing = node.Blockstore("bs_image", "/image")
 bs_landing.size = "500GB"
   
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
   
